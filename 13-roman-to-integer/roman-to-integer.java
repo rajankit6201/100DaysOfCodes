d94b9a50f36ff1e98027a74b104421bb8cc6928b
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-06-08 00:45:46 +0530

    Time: 3 ms (78.39%) | Memory: 46.2 MB (93.31%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,33 +1,31 @@
 class Solution {
-
-    public int romanToInt(String s) {
-
-        int sm = 0;
-
-        for(int i = 0;i<s.length();i++){
-
-            int cur = val(s.charAt(i));
-
-            if(i<s.length()-1&&cur<val(s.charAt(i+1))) sm-= cur;
-            else sm += cur;
-
+    public int getNumber(char n ){
+        switch(n){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D':  return 500;
+            case 'M': return 1000;
         }
-
-        return sm;
+        return 0;
     }
 
-    private int val(char c){
-
-        switch(c){
-            case 'I' : return 1;
-            case 'V' : return 5;
-            case 'X' : return 10;
-            case 'L' : return 50;
-            case 'C' : return 100;
-            case 'D' : return 500;
-            default : return 1000;
-
+    public int romanToInt(String s) {
+        // int I = 1 , V = 5 , X = 10 , L = 50 , C = 100, D = 500 , M = 1000;
+        int n = s.length();
+        char last = s.charAt(n - 1);
+        int count = getNumber(last);
+        for (int i = n - 2; i >= 0; i--) {
+            if (getNumber(s.charAt(i)) >= getNumber(s.charAt(i + 1))) {
+                count += getNumber(s.charAt(i));
+            } else
+                count -= getNumber(s.charAt(i));
         }
+
+        return count;
+
     }
 }
 
