f17cca3085ec8a50811b67ca41bf82ce452ea083
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-06-13 11:34:00 +0530

    Time: 6 ms (25.46%), Space: 47 MB (10.73%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        int wordCount = 0;
+        String ans = "";
+        char digit = 'z';
+        char[] alpha = new char[26];
+
+        // easy to access value 
+        for(int i =0;i<26;i++){
+            alpha[i] = digit;
+            digit--;
+        }
+
+        int n = words.length;
+        for(int i = 0;i<n;i++){
+            String temp = words[i];
+             for(int j = 0;j<temp.length();j++){
+                int idx = (int)(temp.charAt(j) - 'a');
+                wordCount+= weights[idx];
+            }
+            wordCount = wordCount%26;
+            ans = ans+ alpha[wordCount];
+            wordCount =0;
+        }
+        return ans;
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
