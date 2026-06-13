5d8cf76d6879f5fcb34e55e76829c15a7a41f695
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-06-13 22:59:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (85.51%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
