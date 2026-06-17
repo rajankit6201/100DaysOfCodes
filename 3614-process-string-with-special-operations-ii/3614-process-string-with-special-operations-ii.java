3a14e0dc8775e36efcd40507f7bde0517000c1d0
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-06-17 23:58:51 +0530

    Time: 29 ms (73.08%), Space: 48.1 MB (26.92%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public char processStr(String s, long k) {
+        int n = s.length();
+        long len = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '*')
+                len = Math.max(len - 1, 0L);
+            else if (c == '#')
+                len *= 2;
+            else if (c != '%')
+                len++;
+        }
+
+        if (k >= len)
+            return '.';
+
+        for (int i = n - 1; ; i--) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '*':
+                    len++;
+                    break;
+                case '#':
+                    if (k >= len / 2)
+                        k -= len / 2;
+                    len /= 2;
+                    break;
+                case '%':
+                    k = len - 1 - k;
+                    break;
+                default:
+                    if (len == k + 1)
+                        return c;
+                    len--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
