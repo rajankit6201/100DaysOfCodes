def417f730d67e3cecd1409605eea1b6bd1cf57a
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-12 12:40:52 +0530

    Time: 26 ms (99.56%), Space: 77.4 MB (15.51%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int rank = 1;
+        int n = arr.length;
+        int[] ans = new int[n];
+        int idx = 0;
+        for(int ele : arr){
+            ans[idx] = ele;
+            idx++;
+        }
+        Arrays.sort(arr);
+        HashMap<Integer , Integer> map =new HashMap<>();
+        for(int i = 0;i<n;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i] , rank);
+                rank++;
+            }
+        }
+        int i = 0;
+        for(int ele : ans){
+            ans[i] = map.get(ele);
+            i++;
+        }
+        return ans;
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
