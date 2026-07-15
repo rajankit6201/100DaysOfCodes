7d38fd0346c1b88812f719f1602524c480f557ea
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-15 21:15:11 +0530

    Time: 1 ms (83.11%), Space: 42.5 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,28 +1,22 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        if(n==2) return n;
-        int evensum = 0;
-        int oddsum = 0;
-        int i = 1;
-        while(n != 1)
-        {
-            if(n%2==0) evensum = evensum+i;
-            else
-            oddsum= oddsum+i;
-            i++;
-            n--;
-        }
-        int gcd = 1;
-        int min = Math.min(evensum,oddsum);
-        for(int j = min;j>=1;j--)
-        {
-            if(oddsum%j == 0 && evensum%j == 0)
-            {
-                gcd = i;
-                break;
+        
+        int sumOdd = n*n;
+        int sumEven = n*(n+1);
+
+            
+            while(sumEven != 0)
+                {
+                    int temp =  sumEven;
+                    sumEven = sumOdd%sumEven;
+                    sumOdd = temp;
+                }
+            return sumOdd;
             }
-        }
-        return gcd;
         
     }
-}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
