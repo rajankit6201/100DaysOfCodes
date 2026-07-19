d34c48e396e4100935896850877c2fc35248239b
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-19 21:39:29 +0530

    Time: 14 ms (1.43%) | Memory: 44.4 MB (16.63%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0;i<n;i++){
+            for(int j =i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<n;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0;i<n;i++){
+            int left = 0;
+            int right = n-1;
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
