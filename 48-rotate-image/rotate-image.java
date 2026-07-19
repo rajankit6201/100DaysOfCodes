c43a8eb96ecacb7576f4428d2341367215a31606
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-19 21:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (94.07%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -8,12 +8,6 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        for(int i = 0;i<n;i++){
-            for(int j =0;j<n;j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
 
         for(int i = 0;i<n;i++){
             int left = 0;
