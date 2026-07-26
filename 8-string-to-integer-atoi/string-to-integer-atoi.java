0f984e0f3495afc8eab41323c4f1ea46380ebc66
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-27 01:39:03 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (41.98%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,74 +1,45 @@
-// class Solution {
-//     public int myAtoi(String s) {
-        
-//        s =  s.trim();
-//        if(s.length() == 0 ) return 0;
-//        boolean negative = false;
-//        int i = 0;
-//        if(s.charAt(0) == '-'){
-//             negative = true;
-//             i++;
-//        }
-//        else if(s.charAt(0) == '+'){
-//         i++;
-//        }
-//        long ans = 0;
-//        int res = 0;
-//        for(;i<s.length();i++){
-//         char digit = s.charAt(i);
-//         if(digit >= '0' && digit <='9'){
-//             int val = (int)digit - '0';
-//             ans = ans*10 + val;
-//         }
-//         else break;
-
-//        }
-//        if(negative){
-//                 System.out.println(ans);
-//         ans*=-1;
-//        }
-
-//        if(ans < -2147483648 ){
-//         res = -2147483648;
-//        }
-//        else if(ans > 2147483647){
-//         res = 2147483647;
-//        }
-//        else{
-//         res = (int)ans;
-//        }
-//        return res;
-
-//     }
-// }
-
-// // Synced seamlessly with LeetHub Pro
-// // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
-
-
 class Solution {
     public int myAtoi(String s) {
-        
-        int i=0;
-        int sign=1;
-        long res=0;
-        s=s.trim();
-        if(s.isEmpty())return 0;
-        if(s.charAt(i)=='-'|| s.charAt(i)=='+'){
-        sign=(s.charAt(i)=='-') ? -1 : 1;
-        i++;
-        }
-        
-        while(i<s.length() && Character.isDigit(s.charAt(i)) )
-        {
-            res= res*10 +(s.charAt(i)-'0');
-        if (sign * res > Integer.MAX_VALUE ) return Integer.MAX_VALUE;
-        if(sign* res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            
+        int i = 0;
+        int n = s.length();
+
+        // Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
             i++;
         }
-        return (int)res*sign;
 
+        // If string is empty after spaces
+        if (i == n) return 0;
+
+        // Check sign
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            if (s.charAt(i) == '-') sign = -1;
+            i++;
+        }
+
+        long num = 0;
+
+        // Read digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            num = num * 10 + digit;
+
+            // Handle overflow
+            if (sign == 1 && num > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if (sign == -1 && -num < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int) (sign * num);
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
