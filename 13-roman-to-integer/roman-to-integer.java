f692ccc0971e909bb2aee59da1faa2044ba8dcc8
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-07-27 12:09:32 +0530

    Time: 3 ms (78.15%) | Memory: 46.3 MB (75.65%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,31 +1,38 @@
 class Solution {
-    public int getNumber(char n ){
-        switch(n){
+
+    public int getvalue(char ch){
+        switch(ch){
             case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D':  return 500;
-            case 'M': return 1000;
+           
+             case 'V': return 5;
+            
+             case 'X': return 10;
+            
+             case 'L': return 50;
+           
+             case 'C': return 100;
+           
+             case 'D': return 500;
+           
+             case 'M': return 1000;
+           
         }
         return 0;
     }
 
     public int romanToInt(String s) {
-        // int I = 1 , V = 5 , X = 10 , L = 50 , C = 100, D = 500 , M = 1000;
+
         int n = s.length();
-        char last = s.charAt(n - 1);
-        int count = getNumber(last);
-        for (int i = n - 2; i >= 0; i--) {
-            if (getNumber(s.charAt(i)) >= getNumber(s.charAt(i + 1))) {
-                count += getNumber(s.charAt(i));
-            } else
-                count -= getNumber(s.charAt(i));
+        int value = 0;
+        value = value + getvalue(s.charAt(n-1));
+        for(int i = n-2;i>=0;i--){
+            if(getvalue(s.charAt(i)) < getvalue(s.charAt(i+1)))
+            value-=getvalue(s.charAt(i));
+            else{
+                value+=getvalue(s.charAt(i));
+            }
         }
-
-        return count;
-
+        return value;
     }
 }
 
