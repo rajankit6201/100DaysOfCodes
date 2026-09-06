71c32da6e055e808ee60f6a35e633fc7b31dd74a
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-06 11:53:54 +0530

    Time: 90 ms (86.72%) | Memory: 79.7 MB (85.04%) - LeetSync

---
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int sumDecoded(long[] nums) {
+        int n=nums.length;
+        long  sum=0;
+        long MOD = 1000000007L;
+        for(int i=0;i<n;i++){
+            long width =nums[i] %10;
+            long d = nums[i] / 10;
+
+            long t = d;
+            int digits = 0;
+            while (t>0) {
+                digits++;
+                t /= 10;
+            }
+            
+            long divisor =1;
+            for(int j=0;j<digits-width;j++){
+                divisor *= 10;
+            }
+
+            long y = d % divisor;
+            long x = d / divisor;
+
+            long power = calculatePower(x, y, MOD);
+            sum = (sum + power) % MOD;
+        }
+        return (int)sum;
+    }
+    private long calculatePower(long x, long y, long MOD) {
+        long result = 1;
+        while (y > 0) {
+            if (y % 2 == 1) {
+                result = (result * x) % MOD;
+            }
+            x = (x * x) % MOD;
+            y = y / 2;
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
