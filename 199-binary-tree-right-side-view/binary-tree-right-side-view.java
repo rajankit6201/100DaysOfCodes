0eb090d61eb42d9e2e65c9b8038e3d43ad9314a7
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-08 01:16:49 +0530

    Time: 1 ms (71.13%) | Memory: 43.9 MB (15.05%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> li = new ArrayList<>();
+        if (root == null) return li;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+
+                // Last node processed in this level is the rightmost visible node
+                if (i == size - 1) {
+                    li.add(currentNode.val);
+                }
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+
+        return li;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
