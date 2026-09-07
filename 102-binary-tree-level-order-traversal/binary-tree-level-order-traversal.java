1570b00384e87195ef9263a6aa550cad2aaecd3e
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-07 23:03:37 +0530

    Time: 1 ms (95.73%) | Memory: 46.6 MB (80.08%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,40 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-public class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) 
-    {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        List<List<Integer>> res = new LinkedList<List<Integer>>();
-        
-        if(root == null) return res;
-        
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+       List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        while(!queue.isEmpty())
-        {
-            int levelNum = queue.size();
-            List<Integer> subans = new LinkedList<Integer>();
-            for(int i=0; i<levelNum; i++) 
-            {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                subans.add(queue.poll().val);
-            }
-            res.add(subans);
+
+        if (root == null) {
+            return result;
         }
-        return res;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> currentLavelList = new ArrayList<>();
+            for(int i = 0;i<size;i++){
+                TreeNode currentNode = queue.poll();
+                currentLavelList.add(currentNode.val);
+                
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLavelList); 
+        }
+        return result;
     }
 }
 
