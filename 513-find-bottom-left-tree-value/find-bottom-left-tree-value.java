b61b511b5a56815f4342f2ef2bb940d0000c1716
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-08 23:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (11.18%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int maxDepth;
+    private int bottomLeftValue;
+
+    public int findBottomLeftValue(TreeNode root) {
+        maxDepth = -1;
+        bottomLeftValue = 0;
+        dfs(root, 0);
+        return bottomLeftValue;
+    }
+
+    private void dfs(TreeNode current, int depth) {
+        if (current == null) {
+            return;
+        }
+        if (depth > maxDepth) {  // If true, we discovered a new level
+            maxDepth = depth;
+            bottomLeftValue = current.val;
+        }
+        dfs(current.left, depth + 1);
+        dfs(current.right, depth + 1);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
