be027be12b5d308b97201ebaad849638124de25c
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-11 12:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (56.55%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    int maximum = -1001;
+    public int helper(TreeNode root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        if(left<0) left = 0;
+        int right = helper(root.right);
+        if(right<0) right =0;
+        int sum = left+right;
+        if(sum<0) sum = 0;
+        maximum = Math.max(maximum , (sum+root.val));
+        return root.val + Math.max(left, right);
+    }
+
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maximum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
