bcb1397b5935ba8906e7890ac2928c4cfc5eaf43
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-17 13:32:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (15.92%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-110);
+        ListNode ans = dummy;
+        ListNode trav = head;
+        while(trav != null){
+            if(ans.val != trav.val){
+                ans.next = trav;
+                ans = ans.next;
+            }
+            trav= trav.next;
+        }
+        ans.next = null;
+        return dummy.next;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
