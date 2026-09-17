63fe74d5b4c88ef76ec1aeef8ae730a47916ccd5
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-17 13:09:33 +0530

    Time: 1 ms (96.64%) | Memory: 47.4 MB (21.77%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode travel = head;
+        while(travel != null){
+            if(travel.val != val){
+                prev.next = travel;
+                prev = prev.next;
+            }
+            travel = travel.next;
+        }
+        prev.next =null;
+        return dummy.next;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
