650861fcb73a33a9b20dff57d1066d4a24e87484
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-18 17:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (16.45%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode ans = null;
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root ==  null) return null;
+        if(root.val == val){
+            ans = root;
+            return ans;
+        }
+        if(root.val < val) searchBST(root.right,val);
+        else searchBST(root.left,val);
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
