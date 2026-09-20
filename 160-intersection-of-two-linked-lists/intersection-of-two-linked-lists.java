bbbcaa05aceb77205c921e3a24de3b5ed5f80876
ankit kumar <90896339+rajankit6201@users.noreply.github.com>
2026-09-20 16:00:00 +0530

    Time: 9 ms (16.07%) | Memory: 52.4 MB (93.00%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> hash =new HashSet<>();
+        while(headA != null){
+            hash.add(headA);
+            headA = headA.next;
+        }
+        while(headB != null){
+            if(hash.contains(headB))return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
